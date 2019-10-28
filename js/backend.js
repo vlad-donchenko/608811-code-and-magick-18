5387dfc1dbc60e73205244dd7bed0b071d0d7135
vlad-donchenko <2pi14b.donchenko@gmail.com>
2019-10-28 23:44:47 +0200

    module6-task1 - реализовал получения данных  и отправку формы на сервер черех AJAX

---
+++ js/backend.js
@@ -0,0 +1,64 @@
+'use strict';
+
+(function () {
+  var URL_LOAD = 'https://js.dump.academy/code-and-magick/data';
+  var URL_SAVE = 'https://js.dump.academy/code-and-magick';
+  var SUCCESS_STATUS = 200;
+  var TIMEOUT = 10000;
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT;
+    xhr.open('GET', URL_LOAD);
+    xhr.send();
+  };
+
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT;
+    xhr.open('POST', URL_SAVE);
+    xhr.send(data);
+  };
+
+  window.backend = {
+    load: load,
+    save: save,
+  };
+
+})();
--- js/setup.js
+++ js/setup.js
@@ -9,10 +9,7 @@
   var setupSimilarList = setupSimilar.querySelector('.setup-similar-list');
   setupSimilar.classList.remove('hidden');
   setupSimilar.classList.remove('hidden');
-  var wizards = [];
   var wizardTemplate = document.querySelector('#similar-wizard-template').content;
-  var playersNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var playersSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var coatsColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   var fireballsColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -27,19 +24,17 @@
   var colorCount = 1;
   var setupDefoltTop = window.getComputedStyle(setup, null).getPropertyValue('top');
   var setupDefoltLeft = window.getComputedStyle(setup, null).getPropertyValue('left');
+  var form = setup.querySelector('.setup-wizard-form');
 
-
-  var randomInteger = function (min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-  };
-
-  var getPlayer = function () {
-    return {
-      'name': playersNames[randomInteger(0, playersNames.length - 1)] + ' ' + playersSurnames[randomInteger(0, playersSurnames.length - 1)],
-      'coatColor': coatsColors[randomInteger(0, coatsColors.length - 1)],
-      'eyesColor': eyesColors[randomInteger(0, eyesColors.length - 1)],
-    };
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
   };
 
   var getWizardTemplate = function (object) {
@@ -47,17 +42,19 @@
     var itemPlayerName = item.querySelector('.setup-similar-label');
     itemPlayerName.textContent = object.name;
     var itemCoatColor = item.querySelector('.wizard-coat');
-    itemCoatColor.setAttribute('fill', object.coatColor);
+    itemCoatColor.setAttribute('fill', object.colorCoat);
     var itemEyesColor = item.querySelector('.wizard-eyes');
-    itemEyesColor.setAttribute('fill', object.eyesColor);
+    itemEyesColor.setAttribute('fill', object.colorEyes);
     setupSimilarList.appendChild(item);
   };
 
-  for (var i = 0; i < PLAYERS_COUNT; i++) {
-    wizards.push(getPlayer());
-    getWizardTemplate(wizards[i]);
-  }
+  var renderWizard = function (array) {
+    for (var i = 0; i < PLAYERS_COUNT; i++) {
+      getWizardTemplate(array[i]);
+    }
+  };
 
+  window.backend.load(renderWizard, onError);
 
   var onModalEscPress = function (evt) {
     if (evt.keyCode === ESC_KEYCODE && !evt.target.classList.contains('setup-user-name')) {
@@ -123,6 +120,15 @@
 
   wizardFireballColorElement.addEventListener('click', function () {
     onCoatColorClick(wizardFireballColorElement, wizardFireballColorInput, fireballsColors);
+  });
+
+  var onCloseSetupSubmit = function () {
+    setup.classList.add('hidden');
+  };
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), onCloseSetupSubmit, onError);
+    evt.preventDefault();
   });
 
   window.setup = {
